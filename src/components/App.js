a2f369fc06915496cc2c6f66bfd5accce1d58107
Giselle-97 <giselle.cduran@hotmail.com>
2023-07-06 17:24:35 +0200

    added router

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import '../styles/main.scss';
 import imgLogo from '../images/logo-adalab.png';
 import callToApi from '../services/api';
@@ -10,7 +11,8 @@ import logo_nasa from '../images/logo_nasa.jpg';
 import lStorage from '../services/localstorage';
 
 function App() {
-  const [data, setData] = useState(lStorage.get('lsData', {
+  const [data, setData] = useState(
+    lStorage.get('lsData', {
       name: '',
       slogan: '',
       technologies: '',
@@ -19,10 +21,10 @@ function App() {
       desc: '',
       autor: '',
       job: '',
-      image:'',
-      photo:'',
-    }
-  ))
+      image: '',
+      photo: '',
+    })
+  );
 
   const [url, setUrl] = useState('');
 
@@ -61,21 +63,29 @@ function App() {
       image: '',
       photo: '',
     });
-    setUrl('')
-   };
+    setUrl('');
+  };
   return (
     <div className='container'>
       <Header imgLogo={imgLogo} imgNasa={logo_nasa} />
       <main className='main'>
-        <Preview data={data} />
-        <Form
-          data={data}
-          handleChangeForm={handleChangeForm}
-          handleInputForm={handleInputForm}
-          handleReset={handleInputReset}
-          handleSubmit={handleSubmit}
-          url={url}
-        />
+        <Routes>
+          <Route
+            path='/createcard'
+            element={
+              <>
+                <Preview data={data} />
+                <Form
+                  data={data}
+                  handleChangeForm={handleChangeForm}
+                  handleReset={handleInputReset}
+                  handleSubmit={handleSubmit}
+                  url={url}
+                />
+              </>
+            }
+          />
+        </Routes>
       </main>
       <Footer imgLogo={imgLogo} />
     </div>
