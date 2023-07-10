25aab18e107cef6efaf1fd1983de6a7af3e09fb4
Giselle-97 <giselle.cduran@hotmail.com>
2023-07-10 12:15:30 +0200

    added new changes

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,14 @@
 import '../styles/components/landing.scss';
 function Landing() {
-  <>
-    <div divLan>
-      -- particles.js container --
-      <div id='particles-js'></div>
-      <script src='https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js'></script>
-    </div>
-  </>;
+  return (
+    <>
+      <div>
+        <p>hola</p>
+        -- particles.js container --
+        <div id='particles-js'></div>
+        <script src='https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js'></script>
+      </div>
+    </>
+  );
 }
 export default Landing;
