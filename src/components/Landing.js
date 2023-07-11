39535c3c83bbbc814dd856ac8f890a44857f8def
Giselle-97 <giselle.cduran@hotmail.com>
2023-07-11 14:15:39 +0200

    added background animation

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,137 +1,99 @@
 import '../styles/components/landing.scss';
-import iconInfo from '../images/info2.png';
-import iconTeam from '../images/team2.png';
-import { Link } from 'react-router-dom';
-import Particlesbk from 'react-tsparticles';
-function Landing({ particlesJS }) {
-  particlesJS({
-    particles: {
-      number: {
-        value: 160,
-        density: {
-          enable: true,
-          value_area: 800,
-        },
-      },
-      color: {
-        value: '#ffffff',
-      },
-      shape: {
-        type: 'circle',
-        stroke: {
-          width: 0,
-          color: '#000000',
-        },
-        polygon: {
-          nb_sides: 5,
-        },
-        image: {
-          src: 'img/github.svg',
-          width: 100,
-          height: 100,
-        },
-      },
-      opacity: {
-        value: 1,
-        random: true,
-        anim: {
-          enable: true,
-          speed: 1,
-          opacity_min: 0,
-          sync: false,
-        },
-      },
-      size: {
-        value: 3,
-        random: true,
-        anim: {
-          enable: false,
-          speed: 4,
-          size_min: 0.3,
-          sync: false,
-        },
-      },
-      line_linked: {
-        enable: false,
-        distance: 150,
-        color: '#ffffff',
-        opacity: 0.4,
-        width: 1,
-      },
-      move: {
-        enable: true,
-        speed: 1,
-        direction: 'none',
-        random: true,
-        straight: false,
-        out_mode: 'out',
-        bounce: false,
-        attract: {
-          enable: false,
-          rotateX: 600,
-          rotateY: 600,
-        },
-      },
-    },
-    interactivity: {
-      detect_on: 'canvas',
-      events: {
-        onhover: {
-          enable: true,
-          mode: 'bubble',
-        },
-        onclick: {
-          enable: true,
-          mode: 'repulse',
-        },
-        resize: true,
-      },
-      modes: {
-        grab: {
-          distance: 400,
-          line_linked: {
-            opacity: 1,
+import { useCallback } from 'react';
+import Particles from 'react-particles';
+import { loadFull } from 'tsparticles';
+
+const Landing = () => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
+  return (
+    <Particles
+      id='tsparticles'
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: '#000',
           },
         },
-        bubble: {
-          distance: 250,
-          size: 0,
-          duration: 2,
-          opacity: 0,
-          speed: 3,
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: 'push',
+            },
+            onHover: {
+              enable: true,
+              mode: 'repulse',
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
         },
-        repulse: {
-          distance: 400,
-          duration: 0.4,
+        particles: {
+          color: {
+            value: '#934ed2',
+          },
+          links: {
+            color: '#934ed2',
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: 'none',
+            enable: true,
+            outModes: {
+              default: 'bounce',
+            },
+            random: true,
+            speed: 1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 86,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: 'circle',
+          },
+          size: {
+            value: { min: 1, max: 3 },
+          },
         },
-        push: {
-          particles_nb: 4,
-        },
-        remove: {
-          particles_nb: 2,
-        },
-      },
-    },
-    retina_detect: true,
-  });
-  return (
-    <>
-      <div id='particles-js'></div>
-      <div className='headerLanding'>
-        <img className='iconInfo' src={iconInfo} alt='' />
-        <img className='iconTeam' src={iconTeam} alt='' />
-      </div>
-      <div>
-        <Particlesbk />
-        <button className='btnStart'>
-          <Link to='/form' className='btnStart__link'>
-            Empezar
-          </Link>
-        </button>
-        <script src='http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js'></script>
-        <div>{particlesJS()}</div>
-      </div>
-    </>
+        detectRetina: true,
+      }}
+    />
   );
-}
+};
 export default Landing;
