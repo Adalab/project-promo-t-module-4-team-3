93914c36a807ed5e95e0efc405d4788a337a6573
Giselle-97 <giselle.cduran@hotmail.com>
2023-07-11 14:29:57 +0200

    added button

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,6 +2,7 @@ import '../styles/components/landing.scss';
 import { useCallback } from 'react';
 import Particles from 'react-particles';
 import { loadFull } from 'tsparticles';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   const particlesInit = useCallback(async (engine) => {
@@ -17,83 +18,94 @@ const Landing = () => {
   }, []);
 
   return (
-    <Particles
-      id='tsparticles'
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={{
-        background: {
-          color: {
-            value: '#000',
+    <>
+      <Particles
+        id='tsparticles'
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: '#000',
+            },
           },
-        },
-        fpsLimit: 120,
-        interactivity: {
-          events: {
-            onClick: {
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: 'push',
+              },
+              onHover: {
+                enable: true,
+                mode: 'repulse',
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: '#934ed2',
+            },
+            links: {
+              color: '#934ed2',
+              distance: 150,
               enable: true,
-              mode: 'push',
+              opacity: 0.5,
+              width: 1,
             },
-            onHover: {
+            collisions: {
               enable: true,
-              mode: 'repulse',
             },
-            resize: true,
-          },
-          modes: {
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: '#934ed2',
-          },
-          links: {
-            color: '#934ed2',
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: 'none',
-            enable: true,
-            outModes: {
-              default: 'bounce',
-            },
-            random: true,
-            speed: 1,
-            straight: false,
-          },
-          number: {
-            density: {
+            move: {
+              direction: 'none',
               enable: true,
-              area: 800,
+              outModes: {
+                default: 'bounce',
+              },
+              random: true,
+              speed: 1,
+              straight: false,
             },
-            value: 86,
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 86,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: 'circle',
+            },
+            size: {
+              value: { min: 1, max: 3 },
+            },
           },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: 'circle',
-          },
-          size: {
-            value: { min: 1, max: 3 },
-          },
-        },
-        detectRetina: true,
-      }}
-    />
+          detectRetina: true,
+        }}
+      />
+      <div className='btnLanding'>
+        <Link to='/preview' className='btnStart'>
+          <span id='span1'></span>
+          <span id='span2'></span>
+          <span id='span3'></span>
+          <span id='span4'></span>
+          Comenzar
+        </Link>
+      </div>
+    </>
   );
 };
 export default Landing;
