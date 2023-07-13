71b17cdf7277cc04fd9797a9588eee0c7d098117
Jenifer <jaldo.jeny@gmail.com>
2023-07-13 12:35:30 +0200

    add new footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,10 @@
 function Footer({ imgLogo }) {
   return (
     <footer className='footer'>
-      <img className='footer__logo' src={imgLogo} alt='Logo Adalab' />
+      <p class='footer__copy'> &copy;2023</p>
+      <a href='https://adalab.es/bootcamps-online' target='_blank'>
+        <img className='footer__logo' src={imgLogo} alt='Logo Adalab' />
+      </a>
     </footer>
   );
 }
