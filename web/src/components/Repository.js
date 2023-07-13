0fd12c293189a4236a1ca66e39c673214b5f78bf
Lourdesjupo <lourdes.jupo@gmail.com>
2023-07-13 21:11:52 +0200

    modify font size

--- web/src/components/Repository.js
+++ web/src/components/Repository.js
@@ -7,60 +7,7 @@ import { useEffect, useState } from 'react';
 
 function Repository() {
 
-  const dataObject = [
-    {
-      name: 'Adalab',
-      slogan: 'Sigueme y lo verás',
-      technologies: 'Comunicacion',
-      demo: 'teste',
-      repo: 'https://github.com/SuelenGalhardo?tab=repositories',
-      desc: 'Paquita, una de las mejores representantes de actores de España en los 90, se ve buscando nuevas estrellas desesperadamente, después de perder a su mayor cliente.',
-      autor: 'Paquita salas',
-      job: 'MOCATRIZ',
-      image: 'https://idolosolvidados.com/wp-content/uploads/2019/07/paquita-salas-1561634165.jpg',
-      photo:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqORQLuuw8oSdVXctAxDvPI1_1ZQN32BNwHtf1dvqgNVqIYtlff_ZQvlEHvWciaBp8iYE&usqp=CAU',
-    },
-    {
-      name: 'Adalab',
-      slogan: 'Sigueme y lo verás',
-      technologies: 'Comunicacion',
-      demo: 'teste',
-      repo: 'https://github.com/SuelenGalhardo?tab=repositories',
-      desc: 'Paquita, una de las mejores representantes de actores de España en los 90, se ve buscando nuevas estrellas desesperadamente, después de perder a su mayor cliente.',
-      autor: 'Paquita salas',
-      job: 'MOCATRIZ',
-      image: 'https://idolosolvidados.com/wp-content/uploads/2019/07/paquita-salas-1561634165.jpg',
-      photo:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqORQLuuw8oSdVXctAxDvPI1_1ZQN32BNwHtf1dvqgNVqIYtlff_ZQvlEHvWciaBp8iYE&usqp=CAU',
-    },
-    {
-      name: 'Adalab',
-      slogan: 'Sigueme y lo verás',
-      technologies: 'Comunicacion',
-      demo: 'teste',
-      repo: 'https://github.com/SuelenGalhardo?tab=repositories',
-      desc: 'Paquita, una de las mejores representantes de actores de España en los 90, se ve buscando nuevas estrellas desesperadamente, después de perder a su mayor cliente.',
-      autor: 'Paquita salas',
-      job: 'MOCATRIZ',
-      image: 'https://idolosolvidados.com/wp-content/uploads/2019/07/paquita-salas-1561634165.jpg',
-      photo:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqORQLuuw8oSdVXctAxDvPI1_1ZQN32BNwHtf1dvqgNVqIYtlff_ZQvlEHvWciaBp8iYE&usqp=CAU',
-    },
-    {
-      name: 'Adalab',
-      slogan: 'Sigueme y lo verás',
-      technologies: 'Comunicacion',
-      demo: 'teste',
-      repo: 'https://github.com/SuelenGalhardo?tab=repositories',
-      desc: 'Paquita, una de las mejores representantes de actores de España en los 90, se ve buscando nuevas estrellas desesperadamente, después de perder a su mayor cliente.',
-      autor: 'Paquita salas',
-      job: 'MOCATRIZ',
-      image: 'https://idolosolvidados.com/wp-content/uploads/2019/07/paquita-salas-1561634165.jpg',
-      photo:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqORQLuuw8oSdVXctAxDvPI1_1ZQN32BNwHtf1dvqgNVqIYtlff_ZQvlEHvWciaBp8iYE&usqp=CAU',
-    },
-  ];
+  
   const [listProjects, setListProjects ] = useState([])
 
   useEffect(()=>{
